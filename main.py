b7863f389cd46a8fb9a7e8c325f970f6ce4881fd
G-site <httydverse@gmail.com>
2025-11-19 17:35:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import asyncio
-import logging
 from aiogram import Bot
 from aiogram.types import BotCommand, BotCommandScopeDefault
 
@@ -30,7 +29,6 @@ async def main():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     try:
         loop = asyncio.SelectorEventLoop()
         asyncio.set_event_loop(loop)
